10c3d0e5090e38f9820b7b013e98fc0067b0d799
Soyy <81018230+haophuc-tonthat@users.noreply.github.com>
2021-08-17 17:07:21 +0700

    Add responsive

--- js/index.js
+++ js/index.js
@@ -1,109 +1,113 @@
 let hero_slide = document.querySelector("#hero-slide");
 
-      let hero_slide_items = hero_slide.querySelectorAll(".slide");
+let hero_slide_items = hero_slide.querySelectorAll(".slide");
 
-      let hero_slide_index = 0;
+let hero_slide_index = 0;
 
-      let hero_slide_play = true;
+let hero_slide_play = true;
 
-      let hero_slide_control_items = hero_slide.querySelectorAll(
-        ".slide-control-item"
-      );
+let hero_slide_control_items = hero_slide.querySelectorAll(
+  ".slide-control-item"
+);
 
-      let slide_next = hero_slide.querySelector(".slide-next");
+let slide_next = hero_slide.querySelector(".slide-next");
 
-      let slide_prev = hero_slide.querySelector(".slide-prev");
+let slide_prev = hero_slide.querySelector(".slide-prev");
 
-      let header = document.querySelector("header");
+let header = document.querySelector("header");
 
-      showSlide = (index) => {
-        hero_slide.querySelector(".slide.active").classList.remove("active");
-        hero_slide
-          .querySelector(".slide-control-item.active")
-          .classList.remove("active");
-        hero_slide_control_items[index].classList.add("active");
-        hero_slide_items[index].classList.add("active");
-      };
+showSlide = (index) => {
+  hero_slide.querySelector(".slide.active").classList.remove("active");
+  hero_slide
+    .querySelector(".slide-control-item.active")
+    .classList.remove("active");
+  hero_slide_control_items[index].classList.add("active");
+  hero_slide_items[index].classList.add("active");
+};
 
-      nextSlide = () => {
-        hero_slide_index =
-          hero_slide_index + 1 === hero_slide_items.length
-            ? 0
-            : hero_slide_index + 1;
-        showSlide(hero_slide_index);
-      };
+nextSlide = () => {
+  hero_slide_index =
+    hero_slide_index + 1 === hero_slide_items.length ? 0 : hero_slide_index + 1;
+  showSlide(hero_slide_index);
+};
 
-      prevSlide = () => {
-        hero_slide_index =
-          hero_slide_index - 1 < 0
-            ? hero_slide_items.length - 1
-            : hero_slide_index - 1;
-        showSlide(hero_slide_index);
-      };
+prevSlide = () => {
+  hero_slide_index =
+    hero_slide_index - 1 < 0
+      ? hero_slide_items.length - 1
+      : hero_slide_index - 1;
+  showSlide(hero_slide_index);
+};
 
-      slide_next.addEventListener("click", () => nextSlide());
+slide_next.addEventListener("click", () => nextSlide());
 
-      slide_prev.addEventListener("click", () => prevSlide());
+slide_prev.addEventListener("click", () => prevSlide());
 
-      // add event to slide select
-      hero_slide_control_items.forEach((item, index) => {
-        item.addEventListener("click", () => showSlide(index));
-      });
+// add event to slide select
+hero_slide_control_items.forEach((item, index) => {
+  item.addEventListener("click", () => showSlide(index));
+});
 
-      // pause slide when mouse come in slider
-      hero_slide.addEventListener("mouseover", () => (hero_slide_play = false));
+// pause slide when mouse come in slider
+hero_slide.addEventListener("mouseover", () => (hero_slide_play = false));
 
-      // resume slide when mouse leave out slider
-      hero_slide.addEventListener("mouseleave", () => (hero_slide_play = true));
+// resume slide when mouse leave out slider
+hero_slide.addEventListener("mouseleave", () => (hero_slide_play = true));
 
-      setTimeout(() => hero_slide_items[0].classList.add("active"), 200);
+setTimeout(() => hero_slide_items[0].classList.add("active"), 200);
 
-      // auto slide
-      setInterval(() => {
-        if (!hero_slide_play) return;
-        nextSlide();
-      }, 4500);
+// auto slide
+setInterval(() => {
+  if (!hero_slide_play) return;
+  nextSlide();
+}, 4500);
 
-      // change header style when scroll
-      window.addEventListener("scroll", () => {
-        if (
-          document.body.scrollTop > 80 ||
-          document.documentElement.scrollTop > 80
-        ) {
-          header.classList.add("shrink");
-        } else {
-          header.classList.remove("shrink");
-        }
-      });
+// change header style when scroll
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
 
-      // element show on scroll
+// element show on scroll
 
-      let scroll =
-        window.requestAnimationFrame ||
-        function (callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
+let scroll =
+  window.requestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
 
-      let el_to_show = document.querySelectorAll(".show-on-scroll");
+let el_to_show = document.querySelectorAll(".show-on-scroll");
 
-      isElInViewPort = (el) => {
-        let rect = el.getBoundingClientRect();
+isElInViewPort = (el) => {
+  let rect = el.getBoundingClientRect();
 
-        let distance = 200;
+  let distance = 200;
 
-        return (
-          rect.top <=
-          (window.innerHeight - distance ||
-            document.documentElement.clientHeight - distance)
-        );
-      };
+  return (
+    rect.top <=
+    (window.innerHeight - distance ||
+      document.documentElement.clientHeight - distance)
+  );
+};
 
-      loop = () => {
-        el_to_show.forEach((el) => {
-          if (isElInViewPort(el)) el.classList.add("show");
-        });
+loop = () => {
+  el_to_show.forEach((el) => {
+    if (isElInViewPort(el)) el.classList.add("show");
+  });
 
-        scroll(loop);
-      };
+  scroll(loop);
+};
 
-      loop();
+loop();
+
+//
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navLinks = document.getElementsByClassName("main-menu1")[0];
+const navLinks2 = document.getElementsByClassName("main-menu2")[0];
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  navLinks2.classList.toggle("active");
+});

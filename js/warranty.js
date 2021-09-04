b0a28937b802e486084acce907ed7ab4b0224644
myfave-LocChung <loc.chung@myfave.com>
2021-09-04 21:50:04 +0700

    add warrany component

---
+++ js/warranty.js
@@ -0,0 +1,83 @@
+$(async function () {
+  async function getSchemes() {
+    const response = await fetch("./data/warranty-schemes.json");
+
+    return response.json();
+  }
+
+  const schemes = await getSchemes();
+
+  var schemesStr = "<ul>";
+
+  $.each(schemes, function (index, scheme) {
+    schemesStr += `<li class="${
+      index == 0 ? "active" : ""
+    }" value=${index}><img src="${scheme.logo}"
+    /><span>${scheme.brand}</span></li>`;
+  });
+
+  schemesStr += "</ul>";
+
+  $("#brand-list").html(schemesStr);
+
+  // load first main
+  loadMain(schemes[0]);
+
+  $(".history .content .left ul li").each(function () {
+    $(this).click(function () {
+      const activeLi = $(".history .content .left ul li.active");
+
+      const scheme = schemes[$(this).attr("value")];
+
+      if (activeLi) {
+        activeLi.removeClass("active");
+      }
+
+      $(this).addClass("active");
+
+      loadMain(scheme);
+    });
+  });
+});
+
+function loadMain(scheme) {
+  $("#main").html(`
+  <div class="top">
+    <div class="pic">
+      <img
+        src="${scheme.imageTop || "./images/no-image.png"}"
+      />
+    </div>
+    <div class="text">
+      <div class="wrappertext">
+        <p>
+          ${scheme.policy}
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="bottom">
+    <div class="smallpic">
+      <img
+        fade-in=""
+        src="${scheme.imageBottomLeft || "./images/no-image.png"}"
+      />
+    </div>
+
+    <div class="smallpic">
+      <img
+        fade-in=""
+        src="${scheme.imageBottomRight || "./images/no-image.png"}"
+      />
+    </div>
+    <ul class="sub">
+      <li class="">
+        <a
+          class=""
+          href="localhost:8080"
+          ><strong class="">more details</strong>
+        </a>
+      </li>
+    </ul>
+  </div>`);
+}

b926c9f396bf593c3452ad0278aa2d030e0128fc
Soyy <81018230+haophuc-tonthat@users.noreply.github.com>
2021-09-05 11:25:34 +0700

    Update 5/9

--- js/index.js
+++ js/index.js
@@ -1,104 +1,237 @@
+//Load Hero-Slide
 var hero = [];
 
-    $(document).ready(function () {
-      $.ajax({
-        type: "GET",
-        url: "data/index-hero.json",
-        dataType: "json",
-        success: function (data) {
-          hero = data;
-          loadHeroSlides();
-          let hero_slide = document.querySelector("#hero-slide");
+$(document).ready(function () {
+  $.ajax({
+    type: "GET",
+    url: "data/index-hero.json",
+    dataType: "json",
+    success: function (data) {
+      hero = data;
+      loadHeroSlides();
+      let hero_slide = document.querySelector("#hero-slide");
 
-          let hero_slide_items = hero_slide.querySelectorAll(".slide");
+      let hero_slide_items = hero_slide.querySelectorAll(".slide");
 
-          let hero_slide_index = 0;
+      let hero_slide_index = 0;
 
-          let hero_slide_play = true;
+      let hero_slide_play = true;
 
-          let hero_slide_control_items = hero_slide.querySelectorAll(
-            ".slide-control-item"
-          );
+      let hero_slide_control_items = hero_slide.querySelectorAll(
+        ".slide-control-item"
+      );
 
-          let slide_next = hero_slide.querySelector(".slide-next");
+      let slide_next = hero_slide.querySelector(".slide-next");
 
-          let slide_prev = hero_slide.querySelector(".slide-prev");
+      let slide_prev = hero_slide.querySelector(".slide-prev");
 
-          showSlide = (index) => {
-            hero_slide.querySelector(".slide.active").classList.remove("active");
-            hero_slide
-              .querySelector(".slide-control-item.active")
-              .classList.remove("active");
-            hero_slide_control_items[index].classList.add("active");
-            hero_slide_items[index].classList.add("active");
-          };
+      showSlide = (index) => {
+        hero_slide.querySelector(".slide.active").classList.remove("active");
+        hero_slide
+          .querySelector(".slide-control-item.active")
+          .classList.remove("active");
+        hero_slide_control_items[index].classList.add("active");
+        hero_slide_items[index].classList.add("active");
+      };
 
-          nextSlide = () => {
-            hero_slide_index =
-              hero_slide_index + 1 === hero_slide_items.length ? 0 : hero_slide_index + 1;
-            showSlide(hero_slide_index);
-          };
+      nextSlide = () => {
+        hero_slide_index =
+          hero_slide_index + 1 === hero_slide_items.length
+            ? 0
+            : hero_slide_index + 1;
+        showSlide(hero_slide_index);
+      };
 
-          prevSlide = () => {
-            hero_slide_index =
-              hero_slide_index - 1 < 0
-                ? hero_slide_items.length - 1
-                : hero_slide_index - 1;
-            showSlide(hero_slide_index);
-          };
+      prevSlide = () => {
+        hero_slide_index =
+          hero_slide_index - 1 < 0
+            ? hero_slide_items.length - 1
+            : hero_slide_index - 1;
+        showSlide(hero_slide_index);
+      };
 
-          slide_next.addEventListener("click", () => nextSlide());
+      slide_next.addEventListener("click", () => nextSlide());
 
-          slide_prev.addEventListener("click", () => prevSlide());
+      slide_prev.addEventListener("click", () => prevSlide());
 
-          // add event to slide select
-          hero_slide_control_items.forEach((item, index) => {
-            item.addEventListener("click", () => showSlide(index));
-          });
-
-          // pause slide when mouse come in slider
-          hero_slide.addEventListener("mouseover", () => (hero_slide_play = false));
-
-          // resume slide when mouse leave out slider
-          hero_slide.addEventListener("mouseleave", () => (hero_slide_play = true));
-
-          setTimeout(() => hero_slide_items[0].classList.add("active"), 200);
-
-          // auto slide
-          setInterval(() => {
-            if (!hero_slide_play) return;
-            nextSlide();
-          }, 4500);
-        }
-      });
-    });
-
-    function loadHeroSlides() {
-      var html = "";
-      // vòng lặp: duyệt qua từng phần tử trong mảng products
-      $.each(hero, function (index, item) {
-        var s = "";
-        s += "<div class='slide " + item.class + "'>";
-        s += "<div class='slide-txt'>";
-        s += "<div class='slide-title'>";
-        s += "<h3>" + item.title + "</h3>";
-        s += "</div>";
-        s += "<div class='slide-description'>";
-        s += "<p>" + item.description + "</p>";
-        s += "</div>";
-        s += "<div class='slide-btn'>";
-        s += "<a class='hero-btn'>" + item.button + "</a>";
-        s += "</div>";
-        s += "</div>";
-        s += "<div class='slide-img'>";
-        s += "<img src=" + item.imgSrc + "  />";
-        s += "</div>";
-        s += "</div>";
-        html += s;
+      // add event to slide select
+      hero_slide_control_items.forEach((item, index) => {
+        item.addEventListener("click", () => showSlide(index));
       });
 
+      // pause slide when mouse come in slider
+      hero_slide.addEventListener("mouseover", () => (hero_slide_play = false));
 
+      // resume slide when mouse leave out slider
+      hero_slide.addEventListener("mouseleave", () => (hero_slide_play = true));
 
-      // Bỏ chuỗi html vào bên trong thẻ divProducts
-      $("#hero").html(html);
-    }
+      setTimeout(() => hero_slide_items[0].classList.add("active"), 200);
+
+      // auto slide
+      setInterval(() => {
+        if (!hero_slide_play) return;
+        nextSlide();
+      }, 4500);
+    },
+  });
+});
+
+function loadHeroSlides() {
+  var html = "";
+
+  $.each(hero, function (index, item) {
+    var s = "";
+    s += "<div class='slide " + item.class + "'>";
+    s += "<div class='slide-txt'>";
+    s += "<div class='slide-title'>";
+    s += "<h3>" + item.title + "</h3>";
+    s += "</div>";
+    s += "<div class='slide-description'>";
+    s += "<p>" + item.description + "</p>";
+    s += "</div>";
+    s += "<div class='slide-btn'>";
+    s += "<a class='hero-btn'>" + item.button + "</a>";
+    s += "</div>";
+    s += "</div>";
+    s += "<div class='slide-img'>";
+    s += "<img src=" + item.imgSrc + "  />";
+    s += "</div>";
+    s += "</div>";
+    html += s;
+  });
+
+  $("#hero").html(html);
+}
+
+//Load New Models
+var hotModels = [];
+$(document).ready(function () {
+  $.ajax({
+    type: "GET",
+    url: "data/hot-models.json",
+    dataType: "json",
+    success: function (data) {
+      hotModels = data;
+      loadHotModels();
+      let scroll =
+        window.requestAnimationFrame ||
+        function (callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+
+      let el_to_show = document.querySelectorAll(".show-on-scroll");
+
+      isElInViewPort = (el) => {
+        let rect = el.getBoundingClientRect();
+
+        let distance = 200;
+
+        return (
+          rect.top <=
+          (window.innerHeight - distance ||
+            document.documentElement.clientHeight - distance)
+        );
+      };
+
+      loop = () => {
+        el_to_show.forEach((el) => {
+          if (isElInViewPort(el)) el.classList.add("show");
+        });
+
+        scroll(loop);
+      };
+
+      loop();
+    },
+  });
+});
+function loadHotModels() {
+  var html = "";
+
+  $.each(hotModels, function (index, item) {
+    
+    var s = "";
+    s +=    "<div class='product-large to-top show-on-scroll'>";
+    s +=        "<a href='product-detail.html?id=" + item.id + "'>";
+    s +=          "<img src=" + item.productImgRed + " alt=" + item.productName + " />";
+    s +=          "<div class='product-large-content'>"
+    s +=                "<h3 class='product-card-title'>" + item.productName + "</h3>";
+    s +=
+                        "<p class='product-card-info'>$ " +
+                        item.productPrice +
+                        " Starting MSRP</p>";
+    s +=                "<button class='btn'>Explore</button>";
+    s +=          "</div>"
+    s +=        "</a>";
+    s +=    "</div>";
+    html += s;
+  });
+
+  $("#hot-models-item").html(html);
+}
+
+var newModels = [];
+$(document).ready(function () {
+  $.ajax({
+    type: "GET",
+    url: "data/new-models.json",
+    dataType: "json",
+    success: function (data) {
+      newModels = data;
+      loadNewModels();
+      let scroll =
+        window.requestAnimationFrame ||
+        function (callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+
+      let el_to_show = document.querySelectorAll(".show-on-scroll");
+
+      isElInViewPort = (el) => {
+        let rect = el.getBoundingClientRect();
+
+        let distance = 200;
+
+        return (
+          rect.top <=
+          (window.innerHeight - distance ||
+            document.documentElement.clientHeight - distance)
+        );
+      };
+
+      loop = () => {
+        el_to_show.forEach((el) => {
+          if (isElInViewPort(el)) el.classList.add("show");
+        });
+
+        scroll(loop);
+      };
+
+      loop();
+    },
+  });
+});
+function loadNewModels() {
+  var html = "";
+
+  $.each(newModels, function (index, item) {
+    var s = "";
+    s +=    "<div class='product-card to-top show-on-scroll'>";
+    s +=        "<a href='product-detail.html?id=" + item.id + "'>";
+    s +=          "<img src=" + item.productImgWhite + " alt=" + item.productName + " />";
+    s +=          "<div class='product-card-content'>"
+    s +=                "<h3 class='product-card-title'>" + item.productName + "</h3>";
+    s +=
+                        "<p class='product-card-info'>$ " +
+                        item.productPrice +
+                        " Starting MSRP</p>";
+    s +=                "<button class='btn'>Explore</button>";
+    s +=          "</div>"
+    s +=        "</a>";
+    s +=    "</div>";
+    html += s;
+  });
+
+  $("#new-models-items").html(html);
+}
+
--- js/products-detail.js
+++ js/products-detail.js
@@ -1,119 +1,224 @@
-var hero = [];
+//Show Vehicle Specs
+const showMore = document.getElementsByClassName("show-more")[0];
+const zebraTable = document.getElementsByClassName("zebra-table")[0];
+showMore.addEventListener("click", () => {
+  zebraTable.classList.toggle("zebra-show");
+  showMore.classList.toggle("show-more-hidden");
+});
 
-    $(document).ready(function () {
-      $.ajax({
-        type: "GET",
-        url: "data/index-hero.json",
-        dataType: "json",
-        success: function (data) {
-          hero = data;
-          loadHeroSlides();
-          let hero_slide = document.querySelector("#hero-slide");
+//Gallery-Carousel
+const galleryBlock = document.getElementsByClassName("gallery-block")[0];
+const galleryButton = document.getElementsByClassName("gallery-btn")[0];
+galleryButton.addEventListener("click", () => {
+  galleryBlock.classList.toggle("show-gallery");
+});
+var angle = 0;
+function galleryspin(sign) {
+  spinner = document.querySelector("#spinner");
+  if (!sign) {
+    angle = angle + 45;
+  } else {
+    angle = angle - 45;
+  }
+  spinner.setAttribute("style", "transform: rotateY(" + angle + "deg);");
+}
 
-          let hero_slide_items = hero_slide.querySelectorAll(".slide");
+const myJson = new Request("data/new-models.json");
+// Fetch for Slide
+fetch(myJson)
+  .then((response) => {
+    return response.json();
+  })
+  .then((slide) => {
+    console.log(slide.products.NewModels);
+    const html = slide.products.NewModels
+      .map((image) => { 
+            if(image.id == "NewModels01"){    
+              console.log(image.productImg)
+              console.log(image.productImg[i].productImgColor)
+              return `
+              <div class="slide">
+                <div class="slide-txt">
+                  <div class="slide-title">
+                    <h3>${image.productName}</h3>
+                  </div>
+                </div>
+                <div class="slide-img">
+                  for(var i = 0; i < 5; i++){
+                    <img src="${image.productImg[i].productImgColor}" />
+                  }
+                </div>
+              </div>
+              `;                     
+           }
+      })
+      .join("");
+    document.querySelector("#hero").insertAdjacentHTML("afterbegin", html);
+    let hero_slide = document.querySelector("#hero-slide");
 
-          let hero_slide_index = 0;
+    let hero_slide_items = hero_slide.querySelectorAll(".slide");
 
-          let hero_slide_play = true;
+    let hero_slide_index = 0;
 
-          let hero_slide_control_items = hero_slide.querySelectorAll(
-            ".slide-control-item"
-          );
+    let hero_slide_play = true;
 
-          let slide_next = hero_slide.querySelector(".slide-next");
+    let hero_slide_control_items = hero_slide.querySelectorAll(
+      ".slide-control-item"
+    );
 
-          let slide_prev = hero_slide.querySelector(".slide-prev");
+    let slide_next = hero_slide.querySelector(".slide-next");
 
-          showSlide = (index) => {
-            hero_slide.querySelector(".slide.active").classList.remove("active");
-            hero_slide
-              .querySelector(".slide-control-item.active")
-              .classList.remove("active");
-            hero_slide_control_items[index].classList.add("active");
-            hero_slide_items[index].classList.add("active");
-          };
+    let slide_prev = hero_slide.querySelector(".slide-prev");
 
-          nextSlide = () => {
-            hero_slide_index =
-              hero_slide_index + 1 === hero_slide_items.length ? 0 : hero_slide_index + 1;
-            showSlide(hero_slide_index);
-          };
+    showSlide = (index) => {
+      hero_slide.querySelector(".slide.active").classList.remove("active");
+      hero_slide
+        .querySelector(".slide-control-item.active")
+        .classList.remove("active");
+      hero_slide_control_items[index].classList.add("active");
+      hero_slide_items[index].classList.add("active");
+    };
 
-          prevSlide = () => {
-            hero_slide_index =
-              hero_slide_index - 1 < 0
-                ? hero_slide_items.length - 1
-                : hero_slide_index - 1;
-            showSlide(hero_slide_index);
-          };
+    nextSlide = () => {
+      hero_slide_index =
+        hero_slide_index + 1 === hero_slide_items.length
+          ? 0
+          : hero_slide_index + 1;
+      showSlide(hero_slide_index);
+    };
 
-          slide_next.addEventListener("click", () => nextSlide());
+    prevSlide = () => {
+      hero_slide_index =
+        hero_slide_index - 1 < 0
+          ? hero_slide_items.length - 1
+          : hero_slide_index - 1;
+      showSlide(hero_slide_index);
+    };
 
-          slide_prev.addEventListener("click", () => prevSlide());
+    slide_next.addEventListener("click", () => nextSlide());
 
-          // add event to slide select
-          hero_slide_control_items.forEach((item, index) => {
-            item.addEventListener("click", () => showSlide(index));
-          });
+    slide_prev.addEventListener("click", () => prevSlide());
 
-          // pause slide when mouse come in slider
-          hero_slide.addEventListener("mouseover", () => (hero_slide_play = false));
-
-          // resume slide when mouse leave out slider
-          hero_slide.addEventListener("mouseleave", () => (hero_slide_play = true));
-
-          setTimeout(() => hero_slide_items[0].classList.add("active"), 200);
-
-        //   // auto slide
-        //   setInterval(() => {
-        //     if (!hero_slide_play) return;
-        //     nextSlide();
-        //   }, 4500);
-        }
-      });
+    // add event to slide select
+    hero_slide_control_items.forEach((item, index) => {
+      item.addEventListener("click", () => showSlide(index));
     });
 
-    function loadHeroSlides() {
-      var html = "";
-      // vòng lặp: duyệt qua từng phần tử trong mảng products
-      $.each(hero, function (index, item) {
-        var s = "";
-        s += "<div class='slide " + item.class + "'>";
-        s += "<div class='slide-txt'>";
-        s += "<div class='slide-title'>";
-        s += "<h3>" + "2021 Audi SQ7"+ "</h3>";
-        s += "</div>";
-        s += "</div>";
-        s += "<div class='slide-img'>";
-        s += "<img src=" + item.imgSrc + "  />";
-        s += "</div>";
-        s += "</div>";
-        html += s;
-      });
+    // pause slide when mouse come in slider
+    hero_slide.addEventListener("mouseover", () => (hero_slide_play = false));
 
+    // resume slide when mouse leave out slider
+    hero_slide.addEventListener("mouseleave", () => (hero_slide_play = true));
 
+    setTimeout(() => hero_slide_items[0].classList.add("active"), 200);
+  });
 
-      // Bỏ chuỗi html vào bên trong thẻ divProducts
-      $("#hero").html(html);
-    }
+// //Fetch for Price
+// fetch(myJson)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((price) => {
+//     console.log(price.productPrice);
+//     const html = price.productPrice
+//       .map((productPrice) => {
+//         return `
+//           <div class="showroom-financial">
+//             <div class="financial-title">
+//               <span>${productPrice.priceTitle}</span>
+//             </div>
+//             <div class="financial-content">
+//               <span class="financial-icon-dollar">$</span>
+//               <span class="financial-price" >${productPrice.price}</span>
+//             </div>
+//           </div>
+//           `;
+//       })
+//       .join("");
+//     document
+//       .querySelector("#product-price")
+//       .insertAdjacentHTML("afterbegin", html);
+//   });
 
-    //Show Vehicle Specs
-    const showMore = document.getElementsByClassName("show-more")[0];
-    const zebraTable = document.getElementsByClassName("zebra-table")[0];
-    showMore.addEventListener("click", () => {
-    zebraTable.classList.toggle("zebra-show");
-    showMore.classList.toggle("show-more-hidden");
-    });
+// //
+// fetch(myJson)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((feature) => {
+//     console.log(feature.feature);
+//     const html = feature.feature
+//       .map((featureItem) => {
+//         return `
+//         <div class="feature-items">
+//         <div class="feature-item">
+//           <div class="feature-icon">
+//             <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+//               fill="currentColor" stroke="none">
+//               <polygon points="${featureItem.polygonPoint}"></polygon>
+//               <path
+//                 d="${featureItem.featurePath1}">
+//               </path>
+//               <path
+//                 d="${featureItem.featurePath2}">
+//               </path>
+//               <path
+//                 d="${featureItem.featurePath3}">
+//               </path>
+//             </svg>
+//           </div>
+//           <div class="feature-inner">
+//             ${featureItem.featureContent}
+//           </div>
+//         </div>
+//       </div>
+//           `;
+//       })
+//       .join("");
+//     document
+//       .querySelector("#showroom-feature")
+//       .insertAdjacentHTML("afterbegin", html);
+//   });
 
-    //Gallery-Carousel
-    const galleryBlock = document.getElementsByClassName("gallery-block")[0];
-    const galleryButton = document.getElementsByClassName("gallery-btn")[0];
-      galleryButton.addEventListener("click", () => {
-      galleryBlock.classList.toggle("show-gallery");
-    });
-    var angle = 0;
-    function galleryspin(sign) { 
-    spinner = document.querySelector("#spinner");
-    if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
-    spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-    }
+// //Fetch for Gallery
+// fetch(myJson)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((gallery) => {
+//     console.log(gallery.gallery);
+//     const html = gallery.gallery
+//       .map((image) => {
+//         return `
+//               <img src="${image.galleryImg}" alt />
+//             `;
+//       })
+//       .join("");
+//     document.querySelector("#spinner").insertAdjacentHTML("afterbegin", html);
+//   });
+
+// //Fetch for Vehicle Specs
+// fetch(myJson)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((vehicleSpecs) => {
+//     if (vehicleSpecs.id == "NewModels01") {
+//       console.log(vehicleSpecs.vehicleSpecs);
+//       const html = vehicleSpecs.vehicleSpecs
+//         .map((specs) => {
+//           return `
+//           <div class="zebra-table-item ${specs.addClass}">
+//             <div class="zebra-table-item-inner">
+//               <span><strong>${specs.specsName}</strong></span>
+//               <span>${specs.specsContent}</span>
+//             </div>
+//           </div>
+//           `;
+//         })
+//         .join("");
+//       document
+//         .querySelector("#vehicle-specs")
+//         .insertAdjacentHTML("afterbegin", html);
+//     }
+//   });
